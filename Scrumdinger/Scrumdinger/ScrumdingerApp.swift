c96389974b50fd1c307a4e4c0b14475dc3323306
Erich Flock <erich.flock@check24.de>
2022-06-17 17:18:41 +0200

    Create project

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Erich Flock on 17.06.22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
