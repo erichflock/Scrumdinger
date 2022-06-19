ddfaa85010ddef67215e66cb66cd04b106425624
Erich Flock <erich.flock@check24.de>
2022-06-19 18:17:15 +0200

    Create card view

---
+++ Scrumdinger/Scrumdinger/Models/Utils/TrailingIconLabelStyle.swift
@@ -0,0 +1,23 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Erich Flock on 19.06.22.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+    
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
