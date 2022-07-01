16fd90a0443357ae79cb66485aa0bf973a5b1bbd
Erich Flock <erich.flock@check24.de>
2022-07-01 16:44:46 +0200

    Open edit view when user taps edit on navigation

--- Scrumdinger/Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Scrumdinger/Views/DetailView.swift
@@ -11,6 +11,8 @@ struct DetailView: View {
     
     let scrum: DailyScrum
     
+    @State private var isPresentingEditView = false
+    
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
@@ -46,6 +48,24 @@ struct DetailView: View {
             }
         }
         .navigationTitle(scrum.title)
+        .toolbar {
+            Button("Edit") {
+                isPresentingEditView = true
+            }
+        }
+        .sheet(isPresented: $isPresentingEditView) {
+            NavigationView {
+                DetailEditView()
+                    .navigationTitle(scrum.title)
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Cancel") {
+                                isPresentingEditView = false
+                            }
+                        }
+                    }
+            }
+        }
     }
 }
 
