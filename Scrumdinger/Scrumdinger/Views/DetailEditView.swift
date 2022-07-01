6c444ddefadb6a0506b2847bad3f62cec5006e08
Erich Flock <erich.flock@check24.de>
2022-07-01 15:58:44 +0200

    Create DetailEditView

---
+++ Scrumdinger/Scrumdinger/Views/DetailEditView.swift
@@ -0,0 +1,35 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Erich Flock on 01.07.22.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    
+    @State private var data = DailyScrum.Data()
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Meeting Info")) {
+                TextField("Title", text: $data.title)
+                
+                HStack {
+                    Slider(value: $data.lenthInMinutes, in: 5...30, step: 1) {
+                        Text("Length")
+                    }
+                    Spacer()
+                    Text("\(Int(data.lenthInMinutes)) minutes")
+                }
+            }
+        }
+    }
+}
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
