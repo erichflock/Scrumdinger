dce3c2bb7217867913b47713be8e40a1dbb98816
Erich Flock <erich.flock@check24.de>
2022-07-01 17:12:36 +0200

    Create theme picker

---
+++ Scrumdinger/Scrumdinger/Views/ThemePicker.swift
@@ -0,0 +1,28 @@
+//
+//  ThemePicker.swift
+//  Scrumdinger
+//
+//  Created by Erich Flock on 01.07.22.
+//
+
+import SwiftUI
+
+struct ThemePicker: View {
+    
+    @Binding var selection: Theme
+    
+    var body: some View {
+        Picker("Theme", selection: $selection) {
+            ForEach(Theme.allCases) { theme in
+                ThemeView(theme: theme)
+                    .tag(theme)
+            }
+        }
+    }
+}
+
+struct ThemePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemePicker(selection: .constant(.periwinkle))
+    }
+}
