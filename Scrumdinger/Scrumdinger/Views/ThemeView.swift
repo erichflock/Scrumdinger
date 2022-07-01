2e5889eb705c52d247d23ccd0ac4533c5bb6fd4c
Erich Flock <erich.flock@check24.de>
2022-07-01 17:03:05 +0200

    Create theme view

---
+++ Scrumdinger/Scrumdinger/Views/ThemeView.swift
@@ -0,0 +1,31 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by Erich Flock on 01.07.22.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    
+    let theme: Theme
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 4)
+                .fill(theme.mainColor)
+            
+            Label(theme.name, systemImage: "paintpalette")
+                .padding(4)
+        }
+        .foregroundColor(theme.accentColor)
+        .fixedSize(horizontal: false, vertical: true)
+    }
+}
+
+struct ThemeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemeView(theme: .bubblegum)
+    }
+}
