b8f29d0dc96103ebd11d4c6675f5f1b63b9115a2
Erich Flock <erich.flock@check24.de>
2022-09-05 14:44:51 +0200

    Create structure for the ui tests

---
+++ Scrumdinger/ScrumdingerUITests/TestClasses/ScrumdingerUITests.swift
@@ -0,0 +1,21 @@
+//
+//  ScrumdingerUITests.swift
+//  ScrumdingerUITests
+//
+//  Created by Erich Flock on 05.09.22.
+//
+
+import XCTest
+
+class ScrumdingerUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    override func tearDownWithError() throws {}
+
+    func test_addScrum() {
+        
+    }
+}
